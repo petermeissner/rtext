680bd1fa9672b8ee5e81748111f1f7bfcaa8870e
petermeissner <retep.meissner@gmail.com>
2016-05-09 08:18:11 +0200

    text_tokenize() benchmarking

--- benchmarks/benchmark_text_tokenize.R
+++ benchmarks/benchmark_text_tokenize.R
@@ -1,4 +1,3 @@
-library(diffrprojects)
 
 text_tokenize_old <- function(x, regex=NULL, ignore.case=FALSE, fixed=FALSE, useBytes=FALSE, group=c("words", "lines", "paragraphs")){
   tlength <- text_length(x)
@@ -120,7 +119,86 @@ text_tokenize_4 <- function(x, regex=NULL, ignore.case=FALSE, fixed=FALSE, useBy
 
 
 
+#### ====================================================================================
 
+text_tokenize_4find <- function(x, regex=NULL, ignore.case=FALSE, fixed=FALSE, useBytes=FALSE, group=c("words", "lines", "paragraphs")){
+  tlength <- text_length(x)
+
+  found        <- gregexpr(regex, x, ignore.case, fixed, useBytes)
+  found_from   <- found[[1]]
+  found_length <- attributes(found[[1]])$match.length
+  found_to     <- found_length+found_from-1
+
+  data.frame(
+    from  = found_from,
+    to    = found_to,
+    token = "elkmilk is fresh" #substring(x, found_from, found_to)
+  )
+}
+
+
+#### ====================================================================================
+
+text_tokenize_4subs <- function(x, regex=NULL, ignore.case=FALSE, fixed=FALSE, useBytes=FALSE, group=c("words", "lines", "paragraphs")){
+  tlength <- text_length(x)
+
+  found        <- gregexpr(regex, x, ignore.case, fixed, useBytes)
+  found_from   <- found[[1]]
+  found_length <- attributes(found[[1]])$match.length
+  found_to     <- found_length+found_from-1
+
+  data.frame(
+    from  = found_from,
+    to    = found_to,
+    token = substring(x, found_from, found_to)
+  )
+}
+
+
+#### ====================================================================================
+
+
+text_tokenize_5 <-
+  function(
+    x,
+    regex=NULL,
+    ignore.case=FALSE,
+    fixed=FALSE,
+    useBytes=FALSE,
+    group=c("words", "lines", "paragraphs"),
+    non_token=FALSE
+  ){
+  tlength <- text_length(x)
+
+  found        <- gregexpr(regex, x, ignore.case, fixed, useBytes)
+  found_from   <- found[[1]]
+  found_length <- attributes(found[[1]])$match.length
+  found_to     <- found_length+found_from-1
+
+  token <-
+    data.frame(
+      from  = found_from,
+      to    = found_to,
+      token = strsplit(x, regex)
+    )
+
+  if(non_token==TRUE){
+    non_token <-
+      data.frame(
+        from=c(1,token$to+1),
+        to=c(token$from-1, tlength)
+      )
+    non_token <- non_token[non_token$from<=non_token$to,]
+    non_token$token <- mapply(substring, first=non_token$from, last=non_token$to, MoreArgs = list(text=x))
+    non_token$is_token=FALSE
+  }else{
+    non_token <- data.frame()
+  }
+
+  res <- rbind(token, non_token)
+  res <- res[order(res$from),]
+  res
+}
 
 
 
@@ -137,28 +215,19 @@ text_tokenize_4 <- function(x, regex=NULL, ignore.case=FALSE, fixed=FALSE, useBy
 #### ====================================================================================
 #### TESTING ####
 
+library(diffrprojects)
 
-x  <- substring(text_read(dp_tf(1)), 1, 1000)
 regex <- "\\W+"
 ignore.case=FALSE
 fixed=FALSE
 useBytes=FALSE
 group=c("words", "lines", "paragraphs")
 
-bm_res <-
-microbenchmark::microbenchmark(
-  #text_tokenize_old(x, regex),
-  #text_tokenize_2(x, regex),
-  text_tokenize_3(x, regex),
-  text_tokenize_4(x, regex),
-  unit="s", times = 3
-)
-aggregate(bm_res$time, by=list("function"=bm_res[,1]), function(x){round(mean(x)/1000000000,4)})
-
-
 xfull <- text_read(dp_tf(1))
-RES <- list()
-for(i in c(1,10,100,500,1000,5000,10000,15000,20000,30000,50000, 100000, text_length(xfull))){
+RES   <- list()
+stime <- Sys.time()
+
+for(i in round((1:11)^4.58) ){
   x  <- substring(xfull, 1, i)
   regex <- "\\W+"
   ignore.case=FALSE
@@ -169,19 +238,33 @@ for(i in c(1,10,100,500,1000,5000,10000,15000,20000,30000,50000, 100000, text_le
   bm_res <-
     microbenchmark::microbenchmark(
       text_tokenize_old(x, regex),
-      #text_tokenize_2(x, regex),
+      text_tokenize_2(x, regex),
       text_tokenize_3(x, regex),
       text_tokenize_4(x, regex),
+      text_tokenize_4find(x, regex),
+      text_tokenize_4subs(x, regex),
       unit="s", times = 1
     )
-  tmp <- aggregate(bm_res$time, by=list("fun"=bm_res[,1]), function(x){round(mean(x)/1000000000,4)})
-  tmp$N <- i
-  RES <- rbind(RES, tmp)
+  tmp          <- aggregate(list(seconds=bm_res$time), by=list("fun"=bm_res[,1]), function(x){round(mean(x)/1000000000,4)})
+  tmp$seconds2 <- as.numeric(Sys.time()) - as.numeric(stime)
+  tmp$N        <- i
+  RES          <- rbind(RES, tmp)
+  #if(max(RES[,2], na.rm=TRUE)=>3){
+  #  next
+  #}else{
+    cat(i, " ", as.numeric(Sys.time()) - as.numeric(stime), "\n")
+  #}
 }
-RES
+
 
 library(ggplot2)
-ggplot(RES, aes(x=N, y=x, colour=fun)) + geom_line( )
+ggplot(RES, aes(x=N, y=seconds, colour=fun)) +
+  geom_line( ) +
+  geom_hline(yintercept=0:3) +
+  geom_vline(xintercept=c(0,1000000))
+
+RES
+
 
 # load("bm_res_1.Rdata")
 

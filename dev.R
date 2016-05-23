7f7d755de379f6836b7968a796eed9971b00433f
petermeissner <retep.meissner@gmail.com>
2016-05-23 16:21:39 +0200

    test

--- dev.R
+++ dev.R
@@ -1,10 +1,26 @@
 library(diffrprojects)
 
-dongs <- rtext$new(file=dp_tf(1))
+#dongs <- rtext$new(file=dp_tf(1))
 dings <- rtext$new(text="1234567890")
 
+dh_1 <- dings$data_hash()
+dh_2 <- dings$data_hash()
 
-dings$delete(2)$get_text()
+dings$char_add("a")
+dh_3 <- dings$data_hash()
+
+dings$char_delete(1)
+dh_4 <- dings$data_hash()
+
+dings$char_add("a")
+dh_2 <- dings$data_hash()
+
+dings$char_add("a")
+dh_2 <- dings$data_hash()
+
+dings$char_code()
+
+
 
 # get_token()
 

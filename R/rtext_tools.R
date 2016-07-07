1d849b1b9c8f8f6aceed868efeac66a9c75aec2a
petermeissner <retep.meissner@gmail.com>
2016-07-07 15:25:58 +0200

    rtext : plot.rtext()

--- R/rtext_tools.R
+++ R/rtext_tools.R
@@ -126,7 +126,56 @@ load_into <- function(save_file){
 
 
 
-
+#' function for plotting rtext
+#' @export
+#' @param x
+#' @param y
+#' @param char_data
+plot.rtext <-
+  function(
+    rtext,
+    char_data = NULL,
+    lines     = NULL,
+    col       = "#ED4C4CA0",
+    xlim      = c(0, (ceiling(max(x)/10^nchar(max(x))*10))*(10^nchar(max(x))/10) ),
+    ylim      = c(0, max(y)+1 ),
+    ...
+  ){
+    f <- function(name, lines){
+      if(is.null(lines)){
+        rtext$text_lines()[, name]
+      }else{
+        rtext$text_lines()[lines, name]
+      }
+    }
+    x <- f("nchar",  lines)
+    y <- f("line_i", lines)
+    maxy <- max(y)
+    y    <- abs(y-maxy)+1
+    plot(
+      x    = x,
+      y    = y,
+      type = "n",
+      ylab = "line",
+      xlab = "char",
+      xlim = xlim,
+      ylim = ylim,
+      ...,
+      axes=FALSE
+    )
+    axis(1)
+    axis(2,c(max(y),1),c(1,max(y)))
+    box()
+    rect(xleft=0, xright=x, ybottom=y-0.5, ytop=y+0.5, col = "grey", border = "grey", lty=0)
+    if ( !is.null(char_data) ){
+      dat <- rtext$char_data_get()[, c("i",char_data)]
+      lin <- rtext$text_lines()
+      dat_extra <- lin[which_token(dat$i, lin$from, lin$to),]
+      x <- dat$i - dat_extra$from
+      y <- abs(dat_extra$line_i-maxy)+1
+      rect(xleft=x, xright=x+1, ybottom=y-0.5, ytop=y+0.5, col = col, border = col, lty=0)
+    }
+  }
 
 
 
--- dev.R
+++ dev.R
@@ -8,47 +8,17 @@ dp
 
 
 
-self <- rtext$new(text_file=dp_tf(1))
-self$char_data_set("var1", sample(1:10000, 300), 1)
+dings <- rtext$new(text_file=dp_tf(1))
+dings <- rtext$new(text=paste0(sample(c(letters, LETTERS, "\n"), 3000, TRUE), collapse = ""))
 
+dings <- rtext$new(text=c("111","444444444444444","1", "1", "1"))
+dings$char_data_set("var1", c(1:3), 1)
 
-lines <- c(1,1,1)
-
-res   <- character(length(lines))
-lines <- self$text_lines()[lines,]
-from  <- lines$from
-to    <- lines$to
-for( i in seq_along(from) ){
-  res[i] <- self$text_get(from=from[i], to=to[i])
-}
+dings$text_show()
 
 
 
-
-tmp <- self$text_lines()
-self$text_lines_get(1)
-self$text_lines_get(4)
-self$text_lines_get(1:4, TRUE)
-self$text_lines_get(1:4, FALSE)
-
-
-
-
-
-x         <- nchar(dings$text_get(split = "\n"))+1
-char_data <- dings$char_data_get()
-lines     <- data.frame(from=)
-y <- seq_along(x)
-plot(
-  x,y,
-  pch=".",
-  type = "l",
-  col="grey",
-  ylab="line",
-  xlab="char",
-  asp = 10,
-  xlim = c(0, (ceiling(max(x)/10^nchar(max(x))))*10^nchar(max(x)) )
-)
+plot(dings, char_data = "var1")
 
 
 
